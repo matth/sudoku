b4a3d92f8eab100d7202378826e579313d9acde3
matth <matt@matthaynes.net>
2009-03-22 01:16:00 +0000

    Update license info

--- lib/sudoku/solver/simple.rb
+++ lib/sudoku/solver/simple.rb
@@ -9,11 +9,18 @@ module Sudoku
 		# 
 		# Copyright (c) 2009 Matt Haynes
 		# 
-		# Written and maintained by Matt Haynes <matt@matthaynes.net>.
-		#
-		# This program is free software. You can re-distribute and/or
-		# modify this program under the same terms of ruby itself ---
-		# Ruby Distribution License or GNU General Public License.
+		# This program is free software: you can redistribute it and/or modify
+		# it under the terms of the GNU General Public License as published by
+		# the Free Software Foundation, either version 3 of the License, or
+		# (at your option) any later version.
+		# 
+		# This program is distributed in the hope that it will be useful,
+		# but WITHOUT ANY WARRANTY; without even the implied warranty of
+		# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		# GNU General Public License for more details.
+		# 
+		# You should have received a copy of the GNU General Public License
+		# along with this program.  If not, see <http://www.gnu.org/licenses/>
 		#	
 		class Simple
 		
